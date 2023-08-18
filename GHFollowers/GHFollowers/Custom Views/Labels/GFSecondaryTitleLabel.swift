2dc24d9ed27c919dfb8fc0a03b225f5c69ab67df
Julio Bragança <jcbbraganca@gmail.com>
2023-08-18 16:53:49 -0300

    userVC changes

---
+++ GHFollowers/GHFollowers/Custom Views/Labels/GFSecondaryTitleLabel.swift
@@ -0,0 +1,36 @@
+//
+//  GFSecondaryTitleLabel.swift
+//  GHFollowers
+//
+//  Created by Julio Braganca on 18/08/23.
+//
+
+import UIKit
+
+class GFSecondaryTitleLabel: UILabel {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    init(fontSize: CGFloat) {
+        super.init(frame: .zero)
+        font = UIFont.systemFont(ofSize: fontSize, weight: .medium)
+        configure()
+    }
+    
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        textColor                   = .secondaryLabel
+        adjustsFontSizeToFitWidth   = true
+        minimumScaleFactor          = 0.90
+        lineBreakMode               = .byTruncatingTail
+    }
+}
---
+++ GHFollowers/GHFollowers/Utilities/constants.swift
@@ -0,0 +1,12 @@
+//
+//  constants.swift
+//  GHFollowers
+//
+//  Created by Julio Braganca on 18/08/23.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
