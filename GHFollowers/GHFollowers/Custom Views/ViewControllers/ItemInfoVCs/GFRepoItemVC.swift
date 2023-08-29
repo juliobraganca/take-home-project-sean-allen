a99df740764a070e58eecacdd03fadf80daf2b2c
Julio <jcbbraganca@gmail.com>
2023-08-29 17:00:24 -0300

    GFRepoItemVC creation

---
+++ GHFollowers/GHFollowers/Custom Views/ViewControllers/ItemInfoVCs/GFRepoItemVC.swift
@@ -0,0 +1,23 @@
+//
+//  GFRepoItemVC.swift
+//  GHFollowers
+//
+//  Created by Julio Braganca on 29/08/23.
+//
+
+import UIKit
+
+class GFRepoItemVC: GFItemInfoVC {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureItems()
+    }
+    
+    
+    private func configureItems() {
+        itemInfoViewOne.set(itemInfoType: .repos, withCount: user.publicRepos)
+        itemInfoViewTwo.set(itemInfoType: .gists, withCount: user.publicGists)
+        actionButton.set(backgroundColor: .systemPurple, title: "GitHub Profile")
+    }
+}
