d24e379c621165a0c8e80475c40e9ca3ee326ca9
Julio <jcbbraganca@gmail.com>
2023-08-29 16:59:02 -0300

    new function inside GFButton

--- GHFollowers/GHFollowers/Custom Views/Buttons/GFButton.swift
+++ GHFollowers/GHFollowers/Custom Views/Buttons/GFButton.swift
@@ -35,4 +35,9 @@ class GFButton: UIButton {
         titleLabel?.font        = UIFont.preferredFont(forTextStyle: .headline)
     }
     
+    
+    func set(backgroundColor: UIColor, title: String) {
+        self.backgroundColor = backgroundColor
+        setTitle(title, for: .normal)
+    }
 }
