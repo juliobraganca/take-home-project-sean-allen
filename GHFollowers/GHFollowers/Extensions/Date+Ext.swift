247b7ffa70c5e88c656e45b8d9503d7538d4a222
Julio <jcbbraganca@gmail.com>
2023-08-29 17:00:34 -0300

    Date Extension Creation

---
+++ GHFollowers/GHFollowers/Extensions/Date+Ext.swift
@@ -0,0 +1,19 @@
+//
+//  Date+Ext.swift
+//  GHFollowers
+//
+//  Created by Julio Braganca on 29/08/23.
+//
+
+import Foundation
+
+extension Date {
+    
+    func convertToMonthYearFormat() -> String {
+        let dateFormatter           = DateFormatter()
+        dateFormatter.dateFormat    = "MMM yyyy"
+        dateFormatter.locale        = Locale(identifier: "en_US_POSIX")
+        return dateFormatter.string(from: self)
+    }
+    
+}
