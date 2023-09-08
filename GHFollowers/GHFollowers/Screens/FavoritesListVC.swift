d2d72060ad3f6cf78d82039fb5d1b0e5fb98dd95
Julio <jcbbraganca@gmail.com>
2023-09-08 16:50:51 -0300

    Persistance Manager inclusion

--- GHFollowers/GHFollowers/Screens/FavoritesListVC.swift
+++ GHFollowers/GHFollowers/Screens/FavoritesListVC.swift
@@ -12,5 +12,14 @@ class FavoritesListVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBlue // use system to adapt to dark mode.
+        
+        PersistanceManager.retrieveFavorites { result in
+            switch result {
+            case .success(let favorites):
+                print(favorites)
+            case .failure(let error):
+                break
+            }
+        }
     }
 }
